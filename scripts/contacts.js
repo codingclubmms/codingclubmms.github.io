0ef7c7d640a4bda08741fc5222034140f8a15357
JCoderLi <studybadlyforsure@gmail.com>
2017-07-22 16:56:19 -0400

    not done yet, but working on contacts admin jessica li

---
+++ scripts/contacts.js
@@ -0,0 +1,34 @@
+function jl(){
+  document.getElementById("hdr").innerHTML = "Jessica Li";
+  document.getElementById("schoology").innerHTML = "SCHOOLOGY: " + "<a href='https://masonohioschools.schoology.com/user/38814203/info'>Click here.</a>";
+  document.getElementById("s_email").innerHTML = "SCHOOL EMAIL: " + "<a href='mailto:jessicag.li@masonohioschools.com' target='_top'>Click here.</a>";
+
+}
+
+function gr(){
+  document.getElementById("hdr").innerHTML = "Gayatri Rajan";
+  document.getElementById("schoology").innerHTML = "SCHOOLOGY: " + "<a href='https://masonohioschools.schoology.com/user/38815507/info'>Click here.</a>";
+  document.getElementById("s_email").innerHTML = "SCHOOL EMAIL: " + "<a href='mailto:gayatri.rajan@masonohioschools.com' target='_top'>Click here.</a>";
+
+}
+
+function lt(){
+  document.getElementById("hdr").innerHTML = "Luke Trenaman";
+  document.getElementById("schoology").innerHTML = "SCHOOLOGY: " + "";
+  document.getElementById("s_email").innerHTML = "SCHOOL EMAIL: " + "";
+
+}
+
+function sh(){
+  document.getElementById("hdr").innerHTML = "Stephanie Hyatt";
+  document.getElementById("schoology").innerHTML = "SCHOOLOGY: " + "";
+  document.getElementById("s_email").innerHTML = "SCHOOL EMAIL: " + "";
+
+}
+
+function js(){
+  document.getElementById("hdr").innerHTML = "Judi Stewart";
+  document.getElementById("schoology").innerHTML = "SCHOOLOGY: " + "";
+  document.getElementById("s_email").innerHTML = "SCHOOL EMAIL: " + "";
+
+}
