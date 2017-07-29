ab7131b87fcfac22fcdffaaaf749d36d8fde7016
JCoderLi <studybadlyforsure@gmail.com>
2017-07-28 22:59:32 -0400

    fast push omg

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -1,22 +1,32 @@
-var signUp = false;
-var assign = false;
-var question = false;
+var state;
 
-document.getElementById('si').onclick = function() {
-    signUp = true;
-    assign = false;
-    question = false;
-}​;​
 
-document.getElementById('as').onclick = function(){
-    signUp = false;
-    assign = true;
-    question = false
-};
 
-document.getElementById('qu').onclick = function(){
-    signUp = false;
-    assign = false;
-    question = true;
-};
+$(document).ready(function () {
+        $("#si").click(function () {
+            state = "signUp"
+        })
 
+        $('#as').click(function () {
+            state = "assign"
+        })
+
+        $("#qu").click(function () {
+            state = "question"
+        })
+
+});
+
+function setState(state) {
+  switch (state){
+      case "signUp": {
+          document.getElementById("emailSubject").innerHTML = "Sign up <span class='caret'></span>"
+      }
+      case "assign": {
+          document.getElementById("emailSubject").innerHTML = "Assignments <span class='caret'></span>"
+      }
+      case "question": {
+          document.getElementById("emailSubject").innerHTML = "Questions <span class='caret'></span>"
+      }
+  }
+}
