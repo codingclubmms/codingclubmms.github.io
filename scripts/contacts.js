60b9761e3308ee0ffc72c1b992f2839974283012
JCoderLi <studybadlyforsure@gmail.com>
2017-07-29 12:54:50 -0400

    push it my boi :sweat_drops: admin jessica li

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -1,32 +1,52 @@
 var state;
 
 
-
 $(document).ready(function () {
-        $("#si").click(function () {
-            state = "signUp"
-        })
+    $("#si").click(function () {
+        setState("signUp");
+    })
 
-        $('#as').click(function () {
-            state = "assign"
-        })
+    $('#as').click(function () {
+        setState("assign");
+    })
 
-        $("#qu").click(function () {
-            state = "question"
-        })
+    $("#qu").click(function () {
+        setState("question");
+    });
 
 });
 
+function emailPlease(thelink) {
+    $("#mailMe").click(function () {
+        window.open(thelink)
+    })
+}
+
+
 function setState(state) {
-  switch (state){
-      case "signUp": {
-          document.getElementById("emailSubject").innerHTML = "Sign up <span class='caret'></span>"
-      }
-      case "assign": {
-          document.getElementById("emailSubject").innerHTML = "Assignments <span class='caret'></span>"
-      }
-      case "question": {
-          document.getElementById("emailSubject").innerHTML = "Questions <span class='caret'></span>"
-      }
-  }
+    var originLink = "mailto:gayatri.rajan@masonohioschools.com?cc=luke.trenaman@masonohioschools.com;jessicag.li@masonohioschools.com"
+    switch (state) {
+
+        case "signUp":
+            {
+                document.getElementById("emailSubject").innerHTML = "Sign up <span class='caret'></span>";
+                var signUpLink = originLink + "?subject=Sign%20up"; 
+                emailPlease(signUpLink);
+                break;
+            }
+        case "assign":
+            {
+                document.getElementById("emailSubject").innerHTML = "Assignments <span class='caret'></span>"
+                var assignLink = originLink + "?subject=Assignment";
+                emailPlease(assignLink);
+                break;
+            }
+        case "question":
+            {
+                document.getElementById("emailSubject").innerHTML = "Questions <span class='caret'></span>"
+                var questionLink = originLink + "?subject=I%20have%20a%20question"
+                emailPlease(questionLink)
+                break;
+            }
+    }
 }
